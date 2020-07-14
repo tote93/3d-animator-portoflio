409bf5bb2a4f7e6feab12d48fd17ab8a1956ccb8
Jose Luis <joseluis_12193@hotmail.com>
2020-07-14 22:24:19 +0200

    Home and NavBar
    - Finished Home and Navbar.
    - Responsive elements for mobile and desktop

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const menuBarItems = document.querySelectorAll(".header__item");
+const toggleBtn = document.querySelector(".header__toggle");
+const header = document.querySelector("header");
+
+toggleBtn.addEventListener("click", () => {
+    header.classList.toggle("active");
+})
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
